0299df17379aabc8498eceecfbcc810c84c906c2
viorelstavaru <stavaru_viorel@yahoo.com>
2023-04-05 10:10:40 +0300

    retrieving api json from third basketball platform

--- myproject/games/views.py
+++ myproject/games/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+import requests
 
 
 def home_view(request):
-    return HttpResponse("Hello, world")
+    url = "https://api-basketball.p.rapidapi.com/timezone"
+    headers = {
+        "X-RapidAPI-Key": "3abf050e2amsh9e5855714821316p1697a2jsnb693eba29248",
+        "X-RapidAPI-Host": "api-basketball.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers)
+    return HttpResponse(response)
